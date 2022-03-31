3764dff1b0c065be7c2135d672ac4338023c4c65
LegalHacker1 <goranbjelica303@gmail.com>
2022-03-31 10:36:17 +0200

    Compleate ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    // This is a single line JavaScript comment
+    /* This is multi-line comment!;
+    This is multi-line comment!;
+    This is multi-line comment!;
+    This is multi-line comment!;
+    This is multi-line comment!;
+    */ 
+    // Only change code above this line
+}
+
+main();
+
+module.exports = main;
